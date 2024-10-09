992f3d2578bb5fe06f505f043efe45fafcdd3cd8
thangavel-21 <thangavelganesan21@gmail.com>
2024-10-09 13:14:20 +0530

    initial commit

---
+++ app/src/main/java/com/example/personalinfo/PersonalInfo.kt
@@ -0,0 +1,16 @@
+package com.example.personalinfo
+
+import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+
+class PersonalInfo: Application() {
+
+//    private lateinit var firstore: FirebaseFirestore
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        firstore = Firebase.firestore
+//    }
+}
